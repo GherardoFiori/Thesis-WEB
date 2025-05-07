aa7adb3c1df398437ec8dcb29ea882cb7f523a12
GherardoFiori <B00144205@mytudublin.ie>
2025-05-07 20:09:07 +0000

    attempt at fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,14 +61,14 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    "https://www.exterminai.com"
+    "https://www.exterminai.com",
     "https://exterminai.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
-    "https://www.exterminai.com"        
+    "https://www.exterminai.com",
     "https://exterminai.com"            
     ]
 
