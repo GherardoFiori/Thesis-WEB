53b959198bd9b101731d3ac5ec0a393f1e063e64
GherardoFiori <B00144205@mytudublin.ie>
2025-05-07 20:26:17 +0000

    more attempts

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from detector import views
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('detector.urls')),
-    path('api/csrf/', ensure_csrf_cookie(views.get_csrf_token)), 
-    path('csrf/', views.get_csrf_token, name='get-csrf'),
-    path('analyze/', views.analyze_extension, name='analyze-extension'),
 ]
--- backend/detector/urls.py
+++ backend/detector/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 urlpatterns = [
-    path('analyze/', views.analyze_extension, name='analyze_extension'),
+    path('csrf/', ensure_csrf_cookie(views.get_csrf_token), name='get-csrf'),
+    path('analyze/', views.analyze_extension, name='analyze-extension'),
 ]
