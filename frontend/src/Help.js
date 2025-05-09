8c734b93856946f1d35d2efd58d7bf9c934da838
GherardoFiori <B00144205@mytudublin.ie>
2025-05-09 18:23:15 +0000

    more help

--- frontend/src/Help.js
+++ frontend/src/Help.js
@@ -13,6 +13,7 @@ function Help() {
 
       <p>At the moment this service only supports Firefox and Google Chrome Browser. In the future Edge and Opera will work too!</p>
       <a href="https://chromewebstore.google.com/">Google Chrome Extension Store</a>
+      <br></br>
       <a href="https://addons.mozilla.org/en-US/firefox/extensions/">Firefox Extension Store</a>
     </div>
   );
